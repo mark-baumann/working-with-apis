036f50bd1fe6f7db7fd20c994cc4e8eac2b056df
Mark Baumann <kontakt@markb.de>
2022-03-07 12:49:55 +0100

    new version

--- cocktail cards api/script.js
+++ cocktail cards api/script.js
@@ -1,9 +1,11 @@
 const cocktail_btn  = document.getElementById('cocktail_btn');
 const suche = document.getElementById('suche');
 
-cocktail_btn.addEventListener('click', getRandomCocktail);
 
-function getRandomCocktail(){
+
+cocktail_btn.addEventListener('click', getCocktail);
+
+function getCocktail(){
     fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + suche.value)
 		.then(res => res.json())
 		.then(data => {
@@ -15,8 +17,12 @@ function getRandomCocktail(){
 
             document.write("<h1>" +data.drinks[i].strDrink + "</h1>");  
             document.write("<img src='" +data.drinks[i].strDrinkThumb + "' height='300' width='300' />");
-            
-            console.log(suche.value);
+            document.write("<p> Beschreibung: "+data.drinks[i].strInstructionsDE + "</p>" );
+            document.write("<p> Zutat 1: "+data.drinks[i].strIngredient1 + "</p>" );
+            document.write("<p> Zutat 2: "+data.drinks[i].strIngredient2 + "</p>" );
+            if (data.drinks[i].strIngredient3 != null) {
+              document.write("<p> Zutat 3: "+data.drinks[i].strIngredient3 + "</p>" );
+            }        
             
            }
     
